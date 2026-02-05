38e411356987ca1c3a5d3c1a10a85aa3b401efb7
HCDani <hockdanield@gmail.com>
2026-02-05 21:46:13 +0100

    Cleaned up some files and moved the css to styles

--- ghafrontend/src/App.js
+++ ghafrontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
-import './App.css';
+import './styles/App.css';
 import AddGreenhouseModal from './AddGreenhouseModal';
 import GreenhouseDetailPage from './pages/GreenhouseDetailPage';
 import { pb } from './pbClient';
